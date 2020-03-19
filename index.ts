5f64fb827f4ba635f67e9eca74cd0c78a735b41f
katzIdo <ido@om2.com>
2020-03-19 09:13:48 +0200

    add race to the key events

--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
 // Import stylesheets
 import './style.css';
-import { fromEvent, iif, from , of} from 'rxjs';
-import {pluck, distinctUntilChanged, debounceTime, mergeMap} from 'rxjs/operators';
+import { fromEvent, iif, from , of, race} from 'rxjs';
+import {tap, pluck, distinctUntilChanged, debounceTime, mergeMap} from 'rxjs/operators';
+
 import {MIN_STR_LENGTH_SEARCH_WIKI, DENOUNCE_SEARCH_MILLISECOND, URL} from './config';
 import {search} from './services/searchService';
 import {SearchResult} from './api/searchService';
 
 const query = search({url : URL});
 const searchResults = document.getElementById('searchResults');
-const search$ = fromEvent(document.getElementById('searchInput'), 'keyup').pipe(
+const search$ = race(
+  fromEvent(document.getElementById('searchInput'), 'keypress'),
+  fromEvent(document.getElementById('searchInput'), 'keyup'),
+  fromEvent(document.getElementById('searchInput'), 'keydown'),
+  ).pipe(
   pluck('target', 'value'),
-  distinctUntilChanged(),
   debounceTime(DENOUNCE_SEARCH_MILLISECOND),
+  distinctUntilChanged(),
   mergeMap(
     (value: string) => 
     iif(()=> value.length >= MIN_STR_LENGTH_SEARCH_WIKI, 
